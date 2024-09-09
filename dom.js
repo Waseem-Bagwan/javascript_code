81f00263dc3fbaeb9426fda3da5b5cedf63981ea
Waseem Bagwan <waseembagwan950@gmail.com>
2024-09-09 13:55:46 +0530

    dom and events

--- dom.js
+++ dom.js
@@ -1,65 +1,24 @@
-//here we gonna create about document object model which is abbreviate know as DOM
-// in dom we access the html code through js 
-/*
-    all the html tags or elements that can be accessed through js that elements are converted or 
-    (treated as)to object and that special objects are called as document.
-    this document is present in window object(these are nothing but the objects and function 
-    belong to browser which can be used by js)
-    this document object include all the html tags in it    
-*/ 
-// console.dir() this dir used to print all the propertise and function related to document or
-// special objects
+//this stuff will gonna print content the every child it belong to 
+// and here it how it goes div.innerText
+// const div = document.querySelector("h2");
+// div.innerText = div.innerText + "is being learn by me"
+// console.dir(div);
 
-let re = document.getElementById("container");
-console.dir(re);
+// let change = document.querySelectorAll(".box");
+// //this is how stuff are change by their index number
+// change[0].innerText = "this is box1"; 
+// change[1].innerText = "this is box2"; 
+// change[2].innerText = "this is box3";
 
-let last = document.getElementsByTagName("body");
-console.log(last);
-//this is used detect the which type of element or document is ,it might be a class , id and tag
-// it used to return the nodelist 
-let para = document.querySelector("p")
-// here we need pass the element as argu and it used to return the first element 
+// it will print tag + content = which mean entire element
+// here how it goes syntatically
+// div.innerHTML
 
-let thisclass = document.querySelectorAll("p");
-// this used to return the all element and we need add it's corresponding symbol to it
-// if it's class then . if it's id then # pretty simple 
-console.log(thisclass);
+// this thing will to manipulate or change the content or tag of html
+//basically how its how done 
+//div.textCONTENT = <div>this will change older content and will be replace with this new one<\div> 
+//we can change the text and tags with innerTEXT and innerHTML
+// textCONTENT used to display the hidden content
 
-/*
-    Now it's about to verify their properties
-    we can get value which mean to verify and we can set value which mean we can manipulate element 
-*/
-
-// tagname = it used to return the tag name this is how it's done 
-// here we have a variable (thisclass.tagname)
-
-console.log(thisclass.tagName)
-
-
-//innerText
-
-// Text Node: This is the actual text inside an element. For example, in the code:
-
-// html
-// Copy code
-// <p>Hello, world!</p>
-// "Hello, world!" is a text node inside the <p> element.
-
-// Comment Node: This is a node for comments in the HTML, which won't be displayed on the page. For example:
-
-// html
-// Copy code
-// <!-- This is a comment -->
-// Anything inside <!-- --> is a comment node.
-
-// Element Node: This is an HTML tag, like <div>, <p>, <span>, etc. Element nodes contain other nodes (like text, comments, or other elements). For example:
-
-// html
-// Copy code
-// <div></div>
-// <div> is an element node.
-//this it's syntax of it 
-//          object    tag  verify which child it has
-console.log(document.body.childNodes)
-let items = document.querySelector("div");
-console.dir (items);
+let ad = document.querySelectorAll(".box");
+ad.addEvent
---
+++ event.js
@@ -0,0 +1,9 @@
+let change = document.querySelector(".box");
+change.addEventListener("click",function(){
+    change.innerHTML = "u have clicked"
+    change.style.backgroundColor = "purple"; 
+    change.style.height = "200px"
+})
+// let form = document.createElement("p");
+// document.append(form);
+
